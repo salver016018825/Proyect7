699fd3e6fec2563eb77d5d5c37e3d38aa8dd90c0
Salver <016018825@upq.edu.mx>
2020-12-28 15:24:36 -0600

    Se agrega menu

--- Proyect7.cpp
+++ Proyect7.cpp
@@ -1,45 +1,70 @@
 /*
-    Los números de empleados van del 1 al 20, pero no se ingresan de forma consecutiva, el usuario lo ingresa aleatoriamente, por ejemplo: primero ingresa el número 5, después ingresa el número 2, etc.
+    Los números de empleados van del 1 al 20, pero no se ingresan de forma consecutiva, el usuario lo ingresa aleatoriamente, por ejemplo:
+    primero ingresa el número 5, después ingresa el número 2, etc.
+    Una vez que los datos estén guardados el usuario desea tener las siguientes funciones:
+
+>> Buscar el nombre de un empleado por su número de empleado.
+
+>> Buscar el número de empleado por su nombre.
+
+>> Saber si un usuario no existe.
+
+>> Saber si hay nombres similares entre sus empleados.
+
+>> Mostrar empleados de forma ascendente por número de empleado.
+
+>> Mostrar empleados de forma descendente por número de empleado.
+
+Crea un programa en C++ que de solución a este problema. Solo utiliza lo aprendido en los módulos anteriores.
 */
 
 #include <iostream>
-
+#include <cstring>
+#include <string.h>
 using namespace std;
 
 int main()
 {
+    char nameEmployed[20][50] = {"","","","","","","","","","","","","","","","","","","",""};
     int cod[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-    char nameEmployed[20][50] = {};
-
+    
     int iInferior = 0, iSuperior = 0, iCentro = 0, clave = 0;
     char bandera = 'F';
 
     iInferior = 0;
-    iSuperior = 19;
+    iSuperior = 20;
 
-    cout<<"enter number employed: ";
-    cin>>clave;
+    int sizeName = 0;
+    bool isnull = false;
+    int option;
+    int code;
 
-    while(iInferior <= iSuperior){
-         
-        iCentro = (iInferior + iSuperior) / 2;
-         
-        if(cod[iCentro] == clave){
-            bandera = 'V';
+    do{
+        cout<<"\n1. add employed"<<endl;
+        cout<<"2. search employed with code"<<endl;
+        cout<<"3. search employed with name"<<endl;
+        cout<<"4. user exits"<<endl;
+        cout<<"5. name similar"<<endl;
+        cout<<"6. list ascend employed"<<endl;
+        cout<<"7. list descent employed"<<endl;
+        cout<<"8. exit"<<endl;
+        cin>>option;
+
+        switch (option)
+        {
+            case 1:
+                cout<<"add\ncod of employed"<<endl;
+                cin>>code;
+                cout<<"enter name:"<<endl;
+                cin>>nameEmployed[code][50];
+                cout<<"code: "<<code<<"\nname: "<<nameEmployed[code+1][50]<<endl;
+                
             break;
-        } else if(cod[iCentro] > clave){
-            iSuperior = iCentro -1;
-        } else {
-            iInferior = iCentro +1;
-        }    
-    }
-     
-    if(bandera == 'V'){
-        cout<<"the codigo  "<<clave<<" is the employed: "<<nameEmployed[iCentro];
-    } else {
-        cout<<"the empleoyed not exits";
-    }
+            case 2:
+                cout<<"2. search employed with code"<<endl;
+            break;
+        }
+    }while(option !=8 && option != 8);
 
-    
     return 0;
 }
